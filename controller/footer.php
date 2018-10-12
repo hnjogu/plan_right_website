8cf3117f8af8b6bbb125373ec6b3adbd796bf3bf
harrugg <harrugg@192.168.0.28>
2018-10-12 18:21:59 +0300

    new

--- controller/footer.php
+++ controller/footer.php
@@ -6,33 +6,7 @@
 
        <!-- Custom Theme JavaScript -->
 <!--     <script src="../../vendor/js/main.js"></script> -->
-    <hr>
-    <!-- Page Content -->
-<div class="footer-bottom">
-    <div class="container text-center">
-        <footer class="footer section-padding">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="footer_copyright">
-                        <p> Plan right &copy;
-                            <script>
-                              var currentDate = new Date(),
-                                  day = currentDate.getDate(),
-                                  month = currentDate.getMonth() + 1,
-                                  year = currentDate.getFullYear();
-                              document.write(day + "/" + month + "/" + year)
-                            </script> 
-                            All rights  Reserved | by Plan Right
-                        </p>
-                    </div>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
-    </div>
-</div>
-        <!---end footers---->
+
 
 	
 </body>
